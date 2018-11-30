25013bac1a878ccec7f782449d9f38a9e21f4c0c
Anna Yuan <annayuan@Annas-MBP.wv.cc.cmu.edu>
2018-11-30 16:10:01 -0500

    Updates

---
+++ precache-manifest.59e061c48105807199523a7d3dfdcf43.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc887973ce14d677cf29a4d2c7a9615f",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/bill.cc887973.jpg"
+  },
+  {
+    "revision": "e09a6fae0d25985de533",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/css/main.9f77eb72.chunk.css"
+  },
+  {
+    "revision": "c22b6e954c0465b26a47acc17f5831ce",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/domestic.c22b6e95.svg"
+  },
+  {
+    "revision": "d89452f576962b1340e9",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/js/1.d89452f5.chunk.js"
+  },
+  {
+    "revision": "c1eed9461782881d7dc9",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/js/runtime~main.c1eed946.js"
+  },
+  {
+    "revision": "eb0d1fd9223730b615bd3d0f05cba91e",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/drops.eb0d1fd9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f24a7cd1849c17ab46f6be3b71338864",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/tap.f24a7cd1.png"
+  },
+  {
+    "revision": "a293faf6a7e73fed6f4dbc9ffb218193",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/recycling.a293faf6.jpg"
+  },
+  {
+    "revision": "76ef6cf8302c91786abca693f1716f08",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/bottle.76ef6cf8.jpg"
+  },
+  {
+    "revision": "78b727c7beb2d28b777f44b8d409b535",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/shower.78b727c7.png"
+  },
+  {
+    "revision": "d067e839bc267bd73c510444081bb39f",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/tableware.d067e839.jpg"
+  },
+  {
+    "revision": "93bc4f01cccea237e406b649846161ba",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/rain.93bc4f01.jpg"
+  },
+  {
+    "revision": "63aefd64937fe013526492d5f997af06",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/plates.63aefd64.jpg"
+  },
+  {
+    "revision": "e09a6fae0d25985de533",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/js/main.e09a6fae.chunk.js"
+  },
+  {
+    "revision": "ab932275dc1bac6859ff0aa8d1abefa3",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/fruits.ab932275.png"
+  },
+  {
+    "revision": "1bd2fc6d6e1a0a79c15a44fa4dd7dc2d",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/blank.1bd2fc6d.png"
+  },
+  {
+    "revision": "cc183c9d8f3f51d084100ce8843ffc65",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/bottle.cc183c9d.png"
+  },
+  {
+    "revision": "ed3e6fd3a8cc84ccf765c35cf969ec7f",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/wrong.ed3e6fd3.png"
+  },
+  {
+    "revision": "b11d445b61066a1f60a1d79bfe5ab3de",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/right.b11d445b.png"
+  },
+  {
+    "revision": "15b5b3fae20b266a98cd8d5668e85c92",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/bath.15b5b3fa.svg"
+  },
+  {
+    "revision": "d38e6d2b252561cc787aebbbc7c291b2",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/tap.d38e6d2b.svg"
+  },
+  {
+    "revision": "bae197737249384dd5409334aec50c95",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/toilet.bae19773.svg"
+  },
+  {
+    "revision": "1ca18397e583bdcdde2c9f41330dc214",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/washing.1ca18397.svg"
+  },
+  {
+    "revision": "15adc0700aaac41c41fd23a9d9ac0f0d",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/agriculture.15adc070.svg"
+  },
+  {
+    "revision": "b686e2cb45c96a8238cd9cce93e6d169",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/media/industry.b686e2cb.svg"
+  },
+  {
+    "revision": "d89452f576962b1340e9",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/static/css/1.510fa3f6.chunk.css"
+  },
+  {
+    "revision": "3b90e9bcea5f718a2f1d9bc18892c21d",
+    "url": "%3Cgithub-username%3E.github.io/%3Cproject-repo%3E/index.html"
+  }
+];
